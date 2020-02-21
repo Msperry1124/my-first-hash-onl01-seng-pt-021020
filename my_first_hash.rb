a4e7ea9847cba5bed5099003268bb8cd27a4d839
Melissa Sperry <sperry.mel@gmail.com>
2020-02-21 16:55:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,10 +19,9 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  hash = {"key1" => "value1"}
+hash["key2"] = "value2"
+# returns "value2"
 
-    shipping_manifest["pearl necklace"] = 1
-    shipping_manifest["gun powder"] = 4
-    shipping_manifest["muskets"] = 2
-    shipping_manifest
+puts hash
 end
